08b36bdbac706f5777b39b4c4e2613e6e19b4d04
Gregor <gregorwatt1@gmail.com>
2024-12-22 23:51:59 +0000

    Add files via upload

---
+++ csi-data.php
@@ -0,0 +1,62 @@
+<?php
+$servername = "localhost";
+
+$dbname = "csi_data";
+$username = "admin";
+$password = "raspberry";
+
+
+function array2csv(array &$array)
+{
+   if (count($array) == 0) {
+     return null;
+   }
+   ob_start();
+   $df = fopen("php://output", 'w');
+   fputcsv($df, array_keys(reset($array)));
+   foreach ($array as $row) {
+     fputcsv($df, $row);
+   }
+   fclose($df);
+   return ob_get_clean();
+}
+function download_send_headers($filename) {
+    // disable caching
+    $now = gmdate("D, d M Y H:i:s");
+    header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+    header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
+    header("Last-Modified: {$now} GMT");
+
+    // force download  
+    header("Content-Type: application/force-download");
+    header("Content-Type: application/octet-stream");
+    header("Content-Type: application/download");
+
+    // disposition / encoding on response body
+    header("Content-Disposition: attachment;filename={$filename}");
+    header("Content-Transfer-Encoding: binary");
+}
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM `CSIData`";
+ 
+if ($result = $conn->query($sql)) {
+	$rows = [];
+	while($row = mysqli_fetch_array($result))
+	{
+    	$rows[] = $row;
+	}
+	download_send_headers("data_export_" . date("Y-m-d") . ".csv");
+	echo array2csv($rows);
+	die();
+    $result->free();
+}
+
+$conn->close();
+?>
